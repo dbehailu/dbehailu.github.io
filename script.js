541d78f2f14adf418513997cfd302249188fc1a8
Dagm <dagmbehailu@gmail.com>
2018-09-29 13:36:33 -0400

    front page javascript animation

--- script.js
+++ script.js
@@ -18,3 +18,34 @@ function deleteContent(callback) {
     }, intervalTime);
 
 }
+
+function addContent(contentToAdd, callback) {
+    var currentIndex = 0;
+
+    var intervalId = setInterval(function() {
+        if (currentIndex == contentToAdd.length) {
+            clearInterval(intervalId);
+
+            if (callback) {
+                setTimeout(callback, callbackPause);
+            }
+        }
+
+        fancyText.innerHTML = contentToAdd.substring(0, currentIndex);
+        currentIndex++;
+    }, intervalTime);
+}
+
+setTimeout(function() {
+    deleteContent(function() {
+        addContent("a web developer", function() {
+            deleteContent(function() {
+                addContent("in love with JavaScript", function() {
+                    deleteContent(function() {
+                        addContent("Dagm");
+                    })
+                });
+            });
+        });
+    });
+}, initialPause);
